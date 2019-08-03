9ed5ba1f6c01f16ae14f19d8b2a6a0d362481e1b
Alexander Lloyd <axl639@student.bham.ac.uk>
2019-08-03 13:53:20 +0100

    feat(bookmark-manager): add @angular/material dependency

--- apps/bookmark-manager/src/app/app.module.ts
+++ apps/bookmark-manager/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent],
   declarations: [AppComponent],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
